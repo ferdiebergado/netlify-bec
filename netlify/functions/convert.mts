879cb4eba9ae7d9f8c0913509c418fdcc65932f2
Ferdie Bergado <ferdiebergado@gmail.com>
2023-11-10 22:19:40 +0800

    working proto

--- netlify/functions/convert.mts
+++ netlify/functions/convert.mts
@@ -7,29 +7,34 @@ const { Workbook } = excel;
 
 export default async (req: Request, context: Context) => {
   const wb = new Workbook();
-  const be = path.join(process.cwd(), "public", "be.xlsx");
-  // await wb.xlsx.readFile(path.join(__dirname, "be.xlsx"));
-  await wb.xlsx.readFile(be);
-  const ws = wb.getWorksheet("BAE-BE-001");
+  // const be = path.join(process.cwd(), "public", "be.xlsx");
+  // await wb.xlsx.readFile(be);
 
-  const prog = ws?.getCell("F3").text;
-  const xlsx = path.join(process.cwd(), "be (1).xlsx");
+  // const xlsx = path.join(process.cwd(), "be (1).xlsx");
   // await wb.xlsx.writeFile(path.join(process.cwd(), "be (1).xlsx"));
 
   // const file = fs.readFileSync(xlsx)
-  const res = new Response();
 
-  res.headers.set("Content-Length", fs.statSync(xlsx).size.toString());
+  const buffer = await req.arrayBuffer();
+  await wb.xlsx.load(buffer);
+  const ws = wb.getWorksheet("BAE-BE-001");
+
+  if (ws) {
+    // prog = ws?.getCell("F3").text;
+
+    ws.getCell("F3").value = "test";
+  }
+
+  const buf2 = await wb.xlsx.writeBuffer();
+  const res = new Response(buf2);
+  const filename = `em-${new Date().getTime()}.xlsx`;
+
+  // res.headers.set("Content-Length", fs.statSync(xlsx).size.toString());
   res.headers.set(
     "Content-Type",
     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
   );
-  res.headers.set("Content-Disposition", "attachment; filename=bebe.xlsx");
-  // res.write(file, 'binary');
-
-  // const res = await fetch(xlsx);
-  // const blob = await res.blob();
-  // const url = URL.createObjectURL(blob);
+  res.headers.set("Content-Disposition", `attachment; filename=${filename}`);
 
   return res;
 };
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,80 @@
-const btnConvert = document.getElementById("convert");
-const fileInput = document.getElementById("excelFile");
+const excelForm = document.forms.namedItem(
+  "excelForm"
+) as HTMLFormElement | null;
+const fileInput = document.getElementById(
+  "excelFile"
+) as HTMLInputElement | null;
 
-btnConvert?.addEventListener("click", async (event) => {
-  event.preventDefault();
-  const formData = new FormData();
-  formData.append("excelFile", <HTMLInputElement>fileInput?.files[0]);
-  const res = await fetch("/convert", {
-    method: "POST",
-    body: formData,
+if (excelForm && fileInput) {
+  excelForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    const selectedFile = fileInput.files?.[0];
+
+    if (selectedFile) {
+      formData.append("excelFile", selectedFile);
+
+      try {
+        const res = await fetch("/convert", {
+          method: "POST",
+          body: formData,
+        });
+
+        if (res.ok) {
+          // const blob = await res.blob();
+          // const url = URL.createObjectURL(blob);
+          // window.location.href = url;
+          // console.log(await res.text());
+
+          // Convert the response body to ArrayBuffer
+          const arrayBuffer = await res.arrayBuffer();
+
+          // Convert the ArrayBuffer to a buffer (Uint8Array)
+          const buffer = new Uint8Array(arrayBuffer);
+
+          // Create a Blob from the buffer
+          const blob = new Blob([buffer], {
+            type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+          });
+
+          // Get the filename from the Content-Disposition header
+          const contentDisposition = res.headers.get("Content-Disposition");
+          const filenameMatch =
+            contentDisposition && contentDisposition.match(/filename="(.+?)"/);
+          const filename = filenameMatch
+            ? filenameMatch[1]
+            : `em-${new Date().getTime()}.xlsx`;
+
+          // Create a download link
+          const downloadLink = document.createElement("a");
+          downloadLink.href = URL.createObjectURL(blob);
+          downloadLink.download = filename;
+
+          // Append the link to the body and trigger the click event
+          document.body.appendChild(downloadLink);
+          downloadLink.click();
+
+          // Clean up: remove the link from the body
+          document.body.removeChild(downloadLink);
+        } else {
+          console.error(
+            "Failed to convert. Server returned:",
+            res.status,
+            res.statusText
+          );
+          // Handle error as needed
+        }
+      } catch (error) {
+        console.error("An error occurred during conversion:", error);
+        // Handle error as needed
+      }
+    } else {
+      console.error("No file selected for conversion.");
+      // Handle case where no file is selected
+    }
   });
-  const blob = await res.blob();
-  const url = URL.createObjectURL(blob);
-  window.location.href = url;
-});
+} else {
+  console.error("Form or file input not found.");
+  // Handle case where button or file input is not found
+}

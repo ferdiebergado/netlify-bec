7aae4e3a5bd1fa175972d1a03b145d2d2148134c
Ferdie Bergado <ferdiebergado@gmail.com>
2023-11-19 10:25:32 +0800

    Added tests

---
+++ src/server/__tests__/utils.test.ts
@@ -0,0 +1,28 @@
+import { isCellFormulaValue, extractResult } from '../utils';
+
+describe('isCellFormulaValue', () => {
+  test('returns true if object is a Cell Formula', () => {
+    const formula = {
+      formula: 'A2',
+      result: undefined,
+    };
+    const actual = isCellFormulaValue(formula);
+    const expected = true;
+
+    expect(actual).toBe(expected);
+  });
+});
+
+describe('extractResult', () => {
+  test('returns the result of a formula from a Cell Formula', () => {
+    const result = 10;
+    const formula = {
+      formula: 'A2',
+      result,
+    };
+    const actual = extractResult(formula);
+    const expected = result;
+
+    expect(actual).toBe(expected);
+  });
+});

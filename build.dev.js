88f242f1736eb7baaba4318f44ec31f2aef3e0f2
ferdie bergado <ferdiebergado@gmail.com>
2024-12-07 13:39:35 +0800

    Converted build scripts to esm modules

--- build.dev.js
+++ build.dev.js
@@ -1,29 +1,28 @@
-const esbuild = require('esbuild');
-const postcss = require('postcss');
-const autoprefixer = require('autoprefixer');
-const postcssPresetEnv = require('postcss-preset-env');
-const { sassPlugin } = require('esbuild-sass-plugin');
+import { context } from 'esbuild';
+import postcss from 'postcss';
+import autoprefixer from 'autoprefixer';
+import postcssPresetEnv from 'postcss-preset-env';
+import { sassPlugin } from 'esbuild-sass-plugin';
 
-esbuild
-  .context({
-    entryPoints: ['src/app.ts'],
-    bundle: true,
-    outdir: 'public',
-    entryNames: '[name]',
-    sourcemap: true,
-    logLevel: 'info',
-    plugins: [
-      sassPlugin({
-        async transform(source, _resolveDir) {
-          const { css } = await postcss([
-            autoprefixer,
-            postcssPresetEnv({ stage: 0 }),
-          ]).process(source, { from: undefined });
-          return css;
-        },
-      }),
-    ],
-  })
+context({
+  entryPoints: ['src/app.ts'],
+  bundle: true,
+  outdir: 'public',
+  entryNames: '[name]',
+  sourcemap: true,
+  logLevel: 'info',
+  plugins: [
+    sassPlugin({
+      async transform(source, _resolveDir) {
+        const { css } = await postcss([
+          autoprefixer,
+          postcssPresetEnv({ stage: 0 }),
+        ]).process(source, { from: undefined });
+        return css;
+      },
+    }),
+  ],
+})
   .then(ctx => {
     ctx.watch();
     console.log('watching...');
--- build.js
+++ build.js
@@ -1,26 +1,24 @@
-const esbuild = require('esbuild');
-const postcss = require('postcss');
-const autoprefixer = require('autoprefixer');
-const postcssPresetEnv = require('postcss-preset-env');
-const { sassPlugin } = require('esbuild-sass-plugin');
+import { build } from 'esbuild';
+import postcss from 'postcss';
+import autoprefixer from 'autoprefixer';
+import postcssPresetEnv from 'postcss-preset-env';
+import { sassPlugin } from 'esbuild-sass-plugin';
 
-esbuild
-  .build({
-    entryPoints: ['src/app.ts'],
-    bundle: true,
-    outdir: 'public',
-    entryNames: '[name]',
-    minify: true,
-    plugins: [
-      sassPlugin({
-        async transform(source, _resolveDir) {
-          const { css } = await postcss([
-            autoprefixer,
-            postcssPresetEnv({ stage: 0 }),
-          ]).process(source, { from: undefined });
-          return css;
-        },
-      }),
-    ],
-  })
-  .catch(() => process.exit(1));
+build({
+  entryPoints: ['src/app.ts'],
+  bundle: true,
+  outdir: 'public',
+  entryNames: '[name]',
+  minify: true,
+  plugins: [
+    sassPlugin({
+      async transform(source, _resolveDir) {
+        const { css } = await postcss([
+          autoprefixer,
+          postcssPresetEnv({ stage: 0 }),
+        ]).process(source, { from: undefined });
+        return css;
+      },
+    }),
+  ],
+}).catch(() => process.exit(1));
